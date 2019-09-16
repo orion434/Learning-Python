78c80c02d8bd1b56ddb879be7957c87f8b0fb875
Gian Luigi <gianluigisomma@gmail.com>
2019-09-16 07:10:28 +0100

    Completed exercise on conditionals

--- Ch2/conditionals.py
+++ Ch2/conditionals.py
@@ -3,12 +3,23 @@
 #
 
 def main():
-  x, y = 10, 100
+  x, y = 1000, 100
   
   # conditional flow uses if, elif, else  
+  if (x<y):
+    st = "x is less than y"
+  elif (x==y):
+    st = "x is equal to y"
+  else:
+    st = "x is more than y"
+
+  print(st)    
 
   # conditional statements let you use "a if C else b"
-  
+ 
+  st = "x<y" if (x<y) else "x>=y"
+  print(st)    
+
 
 if __name__ == "__main__":
   main()
